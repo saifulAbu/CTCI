4c719b19781ec62dc75e7060040d9ae3e1becef8
Saiful Abu <saifulabu25@gmail.com>
2020-03-04 06:53:20 -0600

    adding more stuffs

---
+++ 1.array_strings/1-is-unique.rb
@@ -0,0 +1,15 @@
+# check if a string has all unioue characters
+require 'set'
+
+def is_unioue_using_map(str)
+    set = Set.new()
+    str.each_char do
+        |char|
+        return false if set.include? char
+        set.add char
+    end
+    return true
+end
+
+puts is_unioue_using_map "abcdef"
+puts is_unioue_using_map "abcdefa"
