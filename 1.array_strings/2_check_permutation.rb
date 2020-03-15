b7f0c4862d5543cec13301a306b8c673acba77d2
Saiful Abu <saifulabu25@gmail.com>
2020-03-15 17:53:09 -0500

    adding solution to problem 2

---
+++ 1.array_strings/2_check_permutation.rb
@@ -0,0 +1,24 @@
+def is_permutation(str1, str2)
+  return false if str1.size != str2.size
+
+  char_map = Hash.new(0)
+  str1.each_char do
+    |char|
+    char_map[char] += 1
+  end
+
+  str2.each_char do
+    |char|
+      char_map[char] -= 1
+      return false if char_map[char] < 0
+  end
+  return true
+end
+
+
+puts is_permutation("abc", "cab")
+puts is_permutation("abcd", "abce")
+
+
+
+
